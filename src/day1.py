4888beea355ef768e08b18946d95a6095b88b190
jon cragg <jonnycragg@hotmail.com>
2023-12-01 14:03:59 +0000

    day 1 part 1 done, and part 2 done

--- src/day1.py
+++ src/day1.py
@@ -7,30 +7,55 @@ def readData(file):
             myCoords = []  # Declare an empty list
             # clean line
             cleanline = line.replace(" ", "").replace("\n", "")
-            # point at start of string, check all digits and list of worded numbers from that point
-            # if find any then add this number to coords and move pointer by length of that number found
-            # if no match discard the single char and move to next position
-            for number in allNumbers:
-                if cleanline.startswith(number):
-                    myCoords.append(number)
-                    cleanline.removeprefix(number)
-                else:
-                    cleanline = cleanline[1:]
+            # start at beginning of string, check all digits and list of worded numbers from start of string
+            # if find a match then add this number to coords and remove this number fron start of string and repeat
+            # if no match then discard the single char fron start of string and repeat
+            while cleanline != '':
+                for number in allNumbers:
+                    if cleanline.startswith(number):
+                        # as we populate the coords convert from alpha to digit
+                        myCoords.append(convert(number))
+                        cleanline.removeprefix(number)
+                        break
+                # nothing matches so remove 1 char from start of string and repeat
+                cleanline = cleanline[1:]
 
-            for char in cleanline:
-                if char.isdigit():
-                    myCoords.append(char)
-            print("Ellie, this line =" + cleanline)
-            print("myCoords = " + str(myCoords))
+            #print("Ellie, this line =" + cleanline)
+            #print("myCoords = " + str(myCoords))
             # concatentate first and last digit in myCoords
             allCoords.append(myCoords[0] + myCoords[len(myCoords)-1])
-            print("allCoords = " + str(allCoords))
-        # when end of line catch the last elf
+            #print("allCoords = " + str(allCoords))
     return allCoords
 
+def convert(no):
+    # if found 1-9 then simply return it
+    if no.isdigit():
+        return no
+    else:
+        if (no == 'one'):
+            return '1'
+        elif (no == 'two'):
+            return '2'
+        elif (no == 'three'):
+            return '3'
+        elif (no == 'four'):
+            return '4'
+        elif (no == 'five'):
+            return '5'
+        elif (no == 'six'):
+            return '6'
+        elif (no == 'seven'):
+            return '7'
+        elif (no == 'eight'):
+            return '8'
+        elif no == 'nine':
+            return '9'
+        else:
+            return no
+    return no
 
 if (__name__ == "__main__"):
-    myInput = readData('../data/day1_data')
+    myInput = readData('../data/day1_data2')
     answer = 0
     for coord in myInput:
         answer = answer + int(coord)

02ca88e29ae21bc48286fec82e2e94e8ebc6a78a
jonnycragg <jonnycragg@hotmail.com>
2023-12-04 21:18:06 +0000

    day 4 part 2 done but took some time

--- src/day4.py
+++ src/day4.py
@@ -3,24 +3,37 @@ def readData(file):
     with open(file, 'rt') as myfile:  # Open lorem.txt for reading text.
         for line in myfile:  # For each line of text,
             mygames.append(line.replace("\n", "").split(': '))
-        winnings = 0
+        cardwins = []
+        for num in mygames:
+            cardwins.append(1)
+        count = 0
         for game in mygames:
+            current = count
+            count = count + 1
             game[0] = game[0].replace("Card ", "")
             game[1] = game[1].split(' | ')
             game[1][0] = game[1][0].split(" ")
             game[1][1] = game[1][1].split(" ")
-            game.append(findwinner((game[1])))
-            #winnings = winnings + findwinner(game[1])
-        #print(winnings)
+            winnings = findwinner((game[1]))
+            game.append(winnings)
+            index = count
+            for y in range(cardwins[current]):
+                if winnings > 0:
+                    for x in range(winnings):
+                        cardwins[index] = cardwins[index]+1
+                        index = index + 1
+                index = count
+        print(cardwins)
+        sum = 0
+        for n in cardwins:
+            sum = sum + n
+        print(sum)
     return mygames
 def findwinner(game):
     # check numbers in first list exist in second and if so then calc value and return
     wins = 0
-    value = 0
     mygo = game[0]
     checks = game[1]
-    print(mygo)
-    print(checks)
     for go in mygo:
         for num in checks:
             if go != '':
@@ -30,9 +43,6 @@ def findwinner(game):
 
 if (__name__ == "__main__"):
     myInput = readData('../data/day4_data')
-    # which games possible if only 12 red cubes, 13 green cubes, and 14 blue cubes in bag
-    # so loop through all games and check red green and blue highs
-    # if possible then sum the game numbers and return that number
     print("myInput = " + str(myInput))
-    #myInput = readData('../data/day4_realdata')
-    #print("myInput = " + str(myInput))
+    myInput = readData('../data/day4_realdata')
+    print("myInput = " + str(myInput))
